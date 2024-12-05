5ee2ce291bcf8ad45ff9ed8768ddbd18acda2b3f
DavidVigil <angel.vvigil@hotmail.com>
2024-12-04 22:14:01 -0600

    fix(api_campaign): carefully redone api and set proper validations

--- api_campaign/routes/routes.py
+++ api_campaign/routes/routes.py
@@ -4,19 +4,18 @@ from logger.logger_base import Logger
 from flasgger import swag_from
 
 # Define routes for managing campaigns (add, update, delete, etc.)
-
-class CampaignsRoutes(Blueprint):
+class CampaignRoutes(Blueprint):
     def __init__(self, campaign_service, campaign_schema):
         super().__init__('campaign', __name__)  # Initialize the Blueprint
         self.campaign_service = campaign_service  # Service to handle database operations
         self.campaign_schema = campaign_schema  # Schema to validate the campaign data
         self.register_routes()  # Register the routes (endpoints)
         self.logger = Logger()  # Logger for logging messages
-
+        
     def register_routes(self):
         # Register the HTTP routes for the campaign API
         self.route('/api/v1/campaigns', methods=['GET'])(self.get_campaigns)
-        self.route('/api/v1/campaigns', methods=['POST'])(self.add_campaign)
+        self.route('/api/v1/campaigns', methods=['POST'])(self.add_campaigns)
         self.route('/api/v1/campaigns/<int:campaign_id>', methods=['PUT'])(self.update_campaign)
         self.route('/api/v1/campaigns/<int:campaign_id>', methods=['DELETE'])(self.delete_campaign)
         self.route('/healthcheck', methods=['GET'])(self.healthcheck)
@@ -28,17 +27,16 @@ class CampaignsRoutes(Blueprint):
             500: {'description': 'Internal server error'}
         }
     })
-
     def get_campaigns(self):
         # Get all campaigns from the database
         campaigns = self.campaign_service.get_all_campaigns()
         return jsonify(campaigns), 200  # Return the list of campaigns as JSON
-
+    
     @swag_from({
         'tags': ['Campaigns'],
         'parameters': [
             {
-                'title': 'body',
+                'name': 'body',
                 'in': 'body',
                 'required': True,
                 'schema': {
@@ -53,7 +51,7 @@ class CampaignsRoutes(Blueprint):
                         'endDate': {'type': 'string'},
                         'ql': {'type': 'string'},
                     },
-                    'required': ['title', 'description', 'dm', 'status', 'pc', 'startDate', 'endDate','ql']  # These fields are required
+                    'required': ['title', 'description', 'dm', 'status', 'pc', 'startDate','endDate', 'ql']  # These fields are required
                 }
             }
         ],
@@ -63,53 +61,51 @@ class CampaignsRoutes(Blueprint):
             500: {'description': 'Internal server error'}
         }
     })
-    def add_campaign(self):
+    def add_campaigns(self):
         # Add a new campaign
         try:
             request_data = request.json  # Get the data from the request
 
             if not request_data:
-                return jsonify({'error': 'Invalid data, empty'}), 401  # Check if data is empty
+                return jsonify({'error': 'Invalid data, empty'}), 400  # Check if data is empty
 
             title = request_data.get('title')
             description = request_data.get('description')
             dm = request_data.get('dm')
-            status= request_data.get('status')
+            status = request_data.get('status')
             pc = request_data.get('pc')
             startDate = request_data.get('startDate')
             endDate = request_data.get('endDate')
             ql = request_data.get('ql')
 
-            """ # Validate the data using the schema
+            # Validate the data using the schema
             try:
                 self.campaign_schema.validate_title(title)
                 self.campaign_schema.validate_description(description)
                 self.campaign_schema.validate_dm(dm)
-                self.campaign_schema.validate_sts(status)
+                self.campaign_schema.validate_status(status)
                 self.campaign_schema.validate_pc(pc)
                 self.campaign_schema.validate_startDate(startDate)
                 self.campaign_schema.validate_endDate(endDate)
                 self.campaign_schema.validate_ql(ql)
             except ValidationError as e:
-                return jsonify({'error': f'Invalid data: {e}'}), 402  # Return error if validation fails """
+                return jsonify({'error': f'Invalid data: {e}'}), 400  # Return error if validation fails
 
             # Create the new campaign object
             new_campaign = {
-                "title": title,
-                "description": description,
-                "dm": dm,
-                "status": status,
-                "pc": [
-                    {"characterName": char.strip()} for char in (pc if isinstance(pc, list) else pc.split(", "))
-                ],
-                "startDate": startDate,
-                "endDate": endDate,
-                "ql": ql,
+                'title': title,
+                'description': description,
+                'dm': dm,
+                'status': status,
+                'pc': pc,
+                'startDate': startDate,
+                'endDate': endDate,
+                'ql': ql,
             }
             created_campaign = self.campaign_service.add_campaign(new_campaign)  # Add the campaign to the database
             self.logger.info(f'New campaign: {created_campaign}')  # Log the new campaign creation
             return jsonify(created_campaign), 201  # Return the created campaign as JSON
-
+            
         except Exception as e:
             self.logger.error(f'Error adding a new campaign to the database: {e}')
             return jsonify({'error': f'An error has occurred: {e}'}), 500  # Handle any errors
@@ -118,14 +114,14 @@ class CampaignsRoutes(Blueprint):
         'tags': ['Campaigns'],
         'parameters': [
             {
-                'title': 'campaign_id',
+                'name': 'campaign_id',
                 'in': 'path',
                 'required': True,
                 'type': 'integer',
                 'description': 'ID of the campaign to update'
             },
             {
-                'title': 'body',
+                'name': 'body',
                 'in': 'body',
                 'required': True,
                 'schema': {
@@ -151,7 +147,6 @@ class CampaignsRoutes(Blueprint):
             500: {'description': 'Internal server error'}
         }
     })
-
     def update_campaign(self, campaign_id):
         # Update an existing campaign
         try:
@@ -172,44 +167,15 @@ class CampaignsRoutes(Blueprint):
             # Validate the data
             try:
                 self.campaign_schema.validate_title(title)
-            except ValidationError as e:
-                self.logger.error(f'Error validating title: {e}')
-                return jsonify("Error validating title"), 401  # Return error if validation fails
-            try:
                 self.campaign_schema.validate_description(description)
-            except ValidationError as e:
-                self.logger.error(f'Error validating description: {e}')
-                return jsonify("Error validating description"), 402
-            try:
                 self.campaign_schema.validate_dm(dm)
-            except ValidationError as e:
-                self.logger.error(f'Error validating dm: {e}')
-                return jsonify("Error validating dm"), 403
-            try:
-                self.campaign_schema.validate_sts(status)
-            except ValidationError as e:
-                self.logger.error(f'Error validating status: {e}')
-                return jsonify("Error validating status"), 405
-            try:
+                self.campaign_schema.validate_status(status)
                 self.campaign_schema.validate_pc(pc)
-            except ValidationError as e:
-                self.logger.error(f'Error validating pc: {e}')
-                return jsonify("Error validating pc"), 406
-            """ try:
                 self.campaign_schema.validate_startDate(startDate)
-            except ValidationError as e:
-                return jsonify("Error validating start date"), 407 """
-            try:
                 self.campaign_schema.validate_endDate(endDate)
-            except ValidationError as e:
-                self.logger.error(f'Error validating end date: {e}')
-                return jsonify("Error validating end date"), 408
-            
-            try:
                 self.campaign_schema.validate_ql(ql)
             except ValidationError as e:
-                self.logger.error(f'Error validating ql: {e}')
-                return jsonify("Error validating ql"), 409
+                return jsonify({'error': f'Invalid data: {e}'}), 400  # Return error if validation fails
 
             # Update the campaign object
             update_campaign = {
@@ -218,18 +184,17 @@ class CampaignsRoutes(Blueprint):
                 'description': description,
                 'dm': dm,
                 'status': status,
-                'pc': [{'characterName': char.strip()} for char in (pc if isinstance(pc, list) else pc.split(', '))],
+                'pc': pc,
                 'startDate': startDate,
                 'endDate': endDate,
                 'ql': ql,
             }
-
             updated_campaign = self.campaign_service.update_campaign(campaign_id, update_campaign)  # Update the campaign in the database
             if updated_campaign:
                 return jsonify(update_campaign), 200  # Return the updated campaign as JSON
             else:
                 return jsonify({'error': 'Campaign not found'}), 404  # If campaign not found, return an error
-
+            
         except Exception as e:
             self.logger.error(f'Error updating the campaign in the database: {e}')
             return jsonify({'error': f'Error updating the campaign in the database: {e}'}), 500  # Handle any errors
@@ -238,7 +203,7 @@ class CampaignsRoutes(Blueprint):
         'tags': ['Campaigns'],
         'parameters': [
             {
-                'title': 'campaign_id',
+                'name': 'campaign_id',
                 'in': 'path',
                 'required': True,
                 'type': 'integer',
@@ -255,16 +220,16 @@ class CampaignsRoutes(Blueprint):
         # Delete a campaign by its ID
         try:
             deleted_campaign = self.campaign_service.delete_campaign(campaign_id)  # Delete the campaign from the database
-
+            
             if deleted_campaign:
                 return jsonify(deleted_campaign), 200  # Return the deleted campaign as JSON
             else:
                 return jsonify({'error': 'Campaign not found'}), 404  # If campaign not found, return an error
-
+            
         except Exception as e:
             self.logger.error(f'Error deleting the campaign from the database: {e}')
             return jsonify({'error': f'Error deleting the campaign from the database: {e}'}), 500  # Handle any errors
-
+        
     @swag_from({
         'tags': ['Health'],
         'responses': {
--- api_campaign/schemas/schemas.py
+++ api_campaign/schemas/schemas.py
@@ -1,127 +1,101 @@
-from marshmallow import Schema, fields, validates, ValidationError
+from marshmallow import fields, validates, ValidationError
 import re
-from datetime import datetime
+
 
 # This campaign defines the fields we need to validate
-class CampaignSchema (Schema):
-    title = fields.String(required=True)         # title of the campaign, must be a string and is required
-    description = fields.String(required=True)  # Campaign description, must be a string and required
-    dm = fields.String(required=True)           # Dungeon Master, must be a string and required
-    status = fields.String(required=True)       # Status, must be a string and required
-    # array of player characters, must be their names
-    pc = fields.Nested(fields.String(), required=True)  # Player Characters, must be a string and required
-    startDate = fields.String(required=True)    # Start Date, must be a string and required
-    endDate = fields.String(required=True)      # End Date, must be a string and required
-    ql = fields.String(required=True)           # Quest Log, must be a string and is required
+class CampaignSchema:
+    title = fields.String(required=True)  
+    description = fields.String(required=True)
+    dm = fields.String(required=True)  
+    status = fields.String(required=True)
+    pc = fields.String(required=True)  
+    startDate = fields.String(required=True)  
+    endDate = fields.String(required=True)
+    ql = fields.String(required=True)  
+
 
     @validates('title')
     def validate_title(self, value):
         # Remove leading and trailing spaces
         value = value.strip()
-
+        
         # Check if the field is empty
         if not value:
-            raise ValidationError('Campaign title is required.')
-
-        # Check if the value contains only letters (no numbers or special characters)
-        if not re.match('^[A-Za-z]+$', value):
-            raise ValidationError('Campaign title must contain only letters.')
+            raise ValidationError('Campaign is required.')
+        
+        # Check if the value contains letters. It can also contain numbers, spaces, and common punctuation (, . ' -)
+        if not re.match('^[A-Za-z0-9\\s,.\'-]+$', value):
+            raise ValidationError('Campaign must contain only letters, numbers, spaces, and common punctuation.')
+        
+        
 
     @validates('description')
     def validate_description(self, value):
         # Check if the field is empty
         if not value:
             raise ValidationError('Description is required.')
-
+        
         # Check if the length is no more than 250 characters
-        elif len(value) > 250:
-            raise ValidationError('Description must be no longer than 250 characters.')
+        elif len(value) > 555:
+            raise ValidationError('Description must be no longer than 555 characters.')
 
         # Check if the value contains only letters, spaces, and common punctuation (, . ' -)
         if not re.match('^[A-Za-z\\s,.\'-]+$', value):
             raise ValidationError('Description must contain only letters, spaces, and common punctuation.')
-
+        
     @validates('dm')
     def validate_dm(self, value):
         # Check if the field is empty
         if not value:
             raise ValidationError('Dungeon Master is required.')
-
-        # Check if the length is no more than 250 characters
-        elif len(value) > 200:
-            raise ValidationError('Dungeon Master must be no longer than 200 characters.')
-
-        # Check if the value contains only letters, spaces, and common punctuation (, . ' -)
-        if not re.match('^[A-Za-z\\s,.\'-]+$', value):
-            raise ValidationError('Dungeon Master must contain only letters, spaces, and common punctuation.')
+        # Check if the length is no more than 50 characters
+        elif len(value) > 50:
+            raise ValidationError('Dungeon Master must be no longer than 50 characters.')   
 
     @validates('status')
-    def validate_sts(self, value):
+    def validate_status(self, value):
         # Check if the field is empty
         if not value:
-            raise ValidationError('Status is required.')
-
+            raise ValidationError('Campaign Status is required.')
+        # Check if status is different from the options: pending, ongoing, finished
+        if value not in ['pending', 'ongoing', 'finished']:
+            raise ValidationError('Campaign Status must be one of the following: pending, ongoing, finished.')
+        
     @validates('pc')
     def validate_pc(self, value):
         # Check if the field is empty
         if not value:
-            raise ValidationError('Player Characters is required.')
-
-        # Split the value by commas to check how many characters are selected
-        # characters = value.split(',')
-
-        # Check if the field always contains two elements separated by a comma
-        if len(value) < 2:
-            raise ValidationError('Player Characters must always have two or more selections.')
+            raise ValidationError('At least one Player Character is required.')
 
     @validates('startDate')
     def validate_startDate(self, value):
         # Check if the field is empty
         if not value:
             raise ValidationError('Start Date is required.')
-
-        # Check if the value is a valid date (assuming format MM-DD-YYYY)
-        try:
-            # Try to parse the value as a date (MM-DD-YYYY)
-            start_date = datetime.strptime(value, "%m-%d-%Y")
-        except ValueError:
-            raise ValidationError('Invalid start date format. Use MM-DD-YYYY.')
-
+        # Check if the value is a valid date
+        if not re.match('^\d{2}-\d{2}-\d{4}$', value):
+            raise ValidationError('Start Date must be in the format MM-DD-YYYY.')
+    
     @validates('endDate')
-    def validate_endDate(self, value):
+    def validate_endDate(self, value, startDate):
         # Check if the field is empty
         if not value:
             raise ValidationError('End Date is required.')
-
-        # Check if the value is a valid date (assuming format MM-DD-YYYY)
-        try:
-            # Try to parse the value as a date (MM-DD-YYYY)
-            end_date = datetime.strptime(value, '%m-%d-%Y')
-        except ValueError:
-            raise ValidationError('Invalid end date format. Use MM-DD-YYYY.')
-
-        # Ensure startDate exists (i.e., the campaign has a start date)
-        if hasattr(self, 'startDate') and self.startDate:
-            # Convert startDate to datetime for comparison
-            start_date = datetime.strptime(self.startDate, '%m-%d-%Y')
-
-            # Check if the end date is not before the start date
-            if end_date < start_date:  # Direct comparison of startDate and endDate
-                raise ValidationError('End Date cannot be earlier than Start Date.')
-
+        # Check if the value is a valid date
+        if not re.match('^\d{2}-\d{2}-\d{4}$', value):
+            raise ValidationError('End Date must be in the format MM-DD-YYYY.')
+        # Check if the end date is after the start date
+        if value < startDate:
+            raise ValidationError('End Date must be after the Start Date.')
+    
     @validates('ql')
     def validate_ql(self, value):
-        # Check if the field is empty
-        if not value:
-            raise ValidationError('Quest Log is required.')
-
-        # Check if the length is no more than 200 characters
-        """ elif len(value) > 200:
-            raise ValidationError('Quest Log must be no longer than 200 characters.') """
-
-        # Check if the value contains only letters, spaces, commas, periods, and parentheses
-        if not re.match('^[A-Za-z\\s,.\(\)]+$', value):
-            raise ValidationError('Quest Log must contain only letters, spaces, commas, periods, and parentheses.')
+        # Check if the length is no more than 500 characters
+        elif len(value) > 500:
+            raise ValidationError('Quest Log must be no longer than 500 characters.')
+        # Check if the value contains only letters, spaces, and common punctuation (, . ' -)
+        if value and not re.match('^[A-Za-z\\s,.\'-]+$', value):
+            raise ValidationError('Quest Log must contain only letters, spaces, and common punctuation.')
 
 # Main part of the code that runs when the script is executed
 if __name__ == '__main__':
@@ -131,7 +105,7 @@ if __name__ == '__main__':
     schema = CampaignSchema()  # Create a schema instance to validate data
     
     # Example of calling the validation functions:
-    # schema.validate_role('Hello')  # Uncomment this to test the role validation
+    # schema.validate_title('Hello')  # Uncomment this to test the title validation
     
     # This try-except block will handle errors if validation fails
     # try:
